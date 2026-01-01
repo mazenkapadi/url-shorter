34dcf765712451fb9d9b49c719850ffc9a04906d
Mazen Kapadi <mazen.s.kapadi@gmail.com>
2026-01-01 15:12:02 -0500

    Fixed timestamp.tsx

--- app/urls/timestamp.tsx
+++ app/urls/timestamp.tsx
@@ -1,10 +1,20 @@
 "use client";
 
 export function Timestamp({ date }: { date: string }) {
-  return <>{new Date(date).toLocaleString()}</>;
+  return (
+      <>
+        {new Date(date).toLocaleString(undefined, {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+          hour: "numeric",
+          minute: "2-digit",
+          hour12: true,
+        })}
+      </>
+  );
 }
 
 export function DateOnly({ date }: { date: string }) {
   return <>{new Date(date).toLocaleDateString()}</>;
 }
-
