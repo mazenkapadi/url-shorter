fb952a13a88e0ae4b704d6b8c7b9e5820b5aa8f5
Mazen Kapadi <mazen.s.kapadi@gmail.com>
2025-12-31 22:48:30 -0500

    Analytics fix attempt

--- app/urls/[slug]/page.tsx
+++ app/urls/[slug]/page.tsx
@@ -1,10 +1,6 @@
 // app/urls/[slug]/page.tsx
 import { UrlAnalyticsClient } from "./analytics-client";
 
-export default function UrlAnalyticsPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  return <UrlAnalyticsClient slug={params.slug} />;
+export default function UrlAnalyticsPage() {
+  return <UrlAnalyticsClient />;
 }
